fc2e44106f6cfd24412cad6df356c588e35122d5
Paulo Maciel <pmaciel@magicleap.com>
2021-07-26 13:34:41 -0400

    Deleting not needed Vulkan stuff

--- Vulkan/MySamples/cmake.sh
+++
@@ -1,4 +0,0 @@
-cmake -DCMAKE_BUILD_TYPE=Debug \
--DVULKAN_HEADERS_INSTALL_DIR=/home/MAGICLEAP/pmaciel/CodeExperiments/Vulkan/1.2.176.1/x86_64/include/ \
--DVULKAN_LOADER_INSTALL_DIR=/home/MAGICLEAP/pmaciel/CodeExperiments/Vulkan/1.2.176.1/x86_64/lib/ \
--DCMAKE_INSTALL_PREFIX=install ..
